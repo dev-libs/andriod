55500f5a981903d63cce2f3eff000faf767d9bc4
victor6021 <victor60216021@gmail.com>
2019-08-27 11:00:23 -0700

    added colors and background color

--- app/src/main/java/com/example/devlibs/view/CustomView.kt
+++ app/src/main/java/com/example/devlibs/view/CustomView.kt
@@ -1,8 +1,10 @@
 package com.example.devlibs.view
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.devlibs.R
@@ -16,6 +18,8 @@ class CustomView (context: Context, attrs: AttributeSet?): LinearLayout(context,
         val textWeight = typedArray.getFloat(R.styleable.CustomView_layout_weight, 0f)
         val textSize = typedArray.getDimension(R.styleable.CustomView_textSize, 12f)
         val textLayoutParams = LayoutParams(0, LayoutParams.WRAP_CONTENT, textWeight)
+        val textColor = typedArray.getColor(R.styleable.CustomView_text_color, Color.BLUE)
+        val backGroundColor = typedArray.getColor(R.styleable.CustomView_background_color, Color.BLACK)
         val textView = LayoutInflater.from(context).inflate(R.layout.custom_view, this, false) as TextView
         textView.text = "Welcome to Dev Libs!" +
                 "Fill in the blanks to create a funny story about you!\""
@@ -23,6 +27,8 @@ class CustomView (context: Context, attrs: AttributeSet?): LinearLayout(context,
         textView.textSize = textSize
         textView.layoutParams = textLayoutParams
         typedArray.recycle()
+        textView.setTextColor(textColor)
+        setBackgroundColor(backGroundColor)
 
         orientation = HORIZONTAL
         addView(textView)
