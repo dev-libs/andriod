80723e95f546810b750302b0e88fcbc7c6108f82
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-26 18:13:23 -0600

    DevLibs
    Devlibs build Models

---
+++ DevLibs/app/src/main/java/com/example/devlibs/FirstModel.kt
@@ -0,0 +1,16 @@
+package com.example.devlibs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstModel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_model)
+
+
+
+
+    }
+}
