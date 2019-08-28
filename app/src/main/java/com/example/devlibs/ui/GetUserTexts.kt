8c47f32ffc410eace8bdb008c5eade3193c586c2
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-27 19:56:47 -0600

    Generate the Story
    create story variables to parts of story

--- app/src/main/java/com/example/devlibs/ui/GetUserTexts.kt
+++ app/src/main/java/com/example/devlibs/ui/GetUserTexts.kt
@@ -9,8 +9,14 @@ import com.example.devlibs.R
 import com.example.devlibs.adapter.EditTextsRecyclerViewAdapter
 import com.example.devlibs.model.ListObject
 import kotlinx.android.synthetic.main.activity_get_user_texts.*
+import kotlinx.android.synthetic.main.edittexts_item.*
 
 class GetUserTexts : AppCompatActivity() {
+    companion object{
+        const val KEY_NOUN_ONE = "my string "
+        const val KEY_ADJECTIVE = "my adjective "
+        const val KEY_NOUN_TWO = "my second string"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,12 @@ class GetUserTexts : AppCompatActivity() {
 
         bttn_set_words.setOnClickListener {
             val intent = Intent(this, LastPageActivity::class.java)
+
+            //TODO 1 ! grabbing our editText and passing them into our story (using `key`(companion value)):
+
+            intent.putExtra(KEY_NOUN_ONE, edittext1.text.toString())
+            intent.putExtra(KEY_ADJECTIVE, edittext2.text.toString())
+            intent.putExtra(KEY_NOUN_TWO, edittext3.text.toString())
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/devlibs/ui/LastPageActivity.kt
+++ app/src/main/java/com/example/devlibs/ui/LastPageActivity.kt
@@ -8,9 +8,47 @@ import kotlinx.android.synthetic.main.activity_last_page.*
 
 class LastPageActivity : AppCompatActivity() {
 
+    companion object{
+        const val KEY_STORY_INTRO = "Once upon time there was this dude by the name of"
+        const val KEY_SECOND_SENTANCE = "He was very"
+        const val KEY_THIRD_SENTANCE = "and loved doing some random things!"
+        const val KEY_FOURTH_SENTANCE = "He had a friend by the name of"
+        const val KEY_FINAL_SENTANCE = "And when they did random things together it turned to crazy randomness!"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_last_page)
+
+        //TODO 2 that how we place the edittext here
+
+        val firstNoun = intent.getSerializableExtra(GetUserTexts.KEY_NOUN_ONE)
+        if (firstNoun != " ") {
+            var firstNounText = firstNoun.toString()
+    //        story_text.setText(firstNounText)
+
+            val adjective = intent.getSerializableExtra(GetUserTexts.KEY_ADJECTIVE)
+            if (adjective != " ") {
+                var adjectiveText = adjective.toString()
+
+                val secondNoun = intent.getSerializableExtra(GetUserTexts.KEY_NOUN_TWO)
+                if (secondNoun != " ") {
+                    var secondNounText = secondNoun.toString()
+
+                            //these are just hard coded strings referred in companions above
+                    var introText = KEY_STORY_INTRO
+                    var secondSentance = KEY_SECOND_SENTANCE
+                    var thirdSentance = KEY_THIRD_SENTANCE
+                    var fourthSentance = KEY_FOURTH_SENTANCE
+                    var finalSentance = KEY_FINAL_SENTANCE
+
+
+                    story_text.setText("$introText $firstNounText . \n $secondSentance $adjectiveText , $thirdSentance .  " +
+                            "\n $fourthSentance $secondNounText . $finalSentance" )
+                }
+            }
+        }
+        //note that this is just a button intent
 
         button.setOnClickListener {
             val intent = Intent(this, GetUserTexts::class.java)
